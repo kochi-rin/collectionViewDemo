3f2d8c31844ce4ce6a6e366bcb5f858016083b6d
kochi-rin <kochi.rin@zozo.com>
2019-04-09 15:35:31 +0900

    Initial Commit

---
+++ CollecitonViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollecitonViewDemo
+//
+//  Created by kouchi.rin on 2019/04/09.
+//  Copyright © 2019 kouchi.rin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
